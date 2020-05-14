a524362515750d543538aa999b36afbc54568332
finiox <finiox.bv@gmail.com>
2020-05-14 19:54:08 +0200

    Added project files

---
+++ Assets/Editor/SpriteProcessor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class SpriteProcessor : AssetPostprocessor
+{
+    int pixelsPerUnit = 16;
+
+    void OnPreprocessTexture()
+    {
+        TextureImporter textureImporter = (TextureImporter) assetImporter;
+
+        textureImporter.spritePixelsPerUnit = pixelsPerUnit;
+        textureImporter.filterMode = FilterMode.Point;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 2f;
+
+    Rigidbody2D rigidBody;
+
+    void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 input = Vector3.zero;
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = input.normalized;
+
+        Vector3 movement = direction * speed * Time.fixedDeltaTime;
+
+        rigidBody.MovePosition(transform.position + movement);
+    }
+}
